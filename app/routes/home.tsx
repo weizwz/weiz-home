512f4a3735cec9f51e65473c1b9691537695be62
weizwz <1124725517@qq.com>
2025-07-23 16:38:43 +0800

    更新网站描述

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,9 +3,9 @@ import { PersonalHomepage } from "../components/PersonalHomepage";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "分享编码世界与科技生活 - weizwz" },
+    { title: "分享编码与科技 - weizwz" },
     { name: "description", content: "weizwz 的个人主页 - 全栈开发者，专注于编码世界与科技生活" },
-    { name: "keywords", content: "weizwz, 全栈开发, React, Vue, Nodejs, 个人主页" },
+    { name: "keywords", content: "weizwz, 唯知为之, 唯知笔记, 全栈开发, 前端开发, React, Vue, Nodejs, 个人主页" },
   ];
 }
 
